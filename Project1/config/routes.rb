32094049092d9460cdc2de9758c6cba6df51a0a6
Stephen Smith <paulsmith.contactinfo@gmail.com>
2023-04-02 12:20:41 -0700

    fixed routes

--- Project1/config/routes.rb
+++ Project1/config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
-  get 'session/index'
-  get 'session/create'
-  get 'session/show'
-  get 'session/update'
-  get 'session/destroy'
-  get 'reimbursement/index'
-  get 'reimbursement/create'
-  get 'reimbursement/show'
-  get 'reimbursement/update'
-  get 'reimbursement/destroy'
-  get 'personnel/index'
-  get 'personnel/create'
-  get 'personnel/show'
-  get 'personnel/update'
-  get 'personnel/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post 'auth/login', to: 'session#create'
+  resources :personnel, only: [:index, :create, :show, :update, :destroy]
+  resources :reimbursement, only: [:index, :create, :show, :update, :destroy]
+
 end
