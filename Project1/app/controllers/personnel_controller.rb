6ddb4b001467f81f4a09c873d1d91db45c2fa364
Paul Smith <paulsmith.contactinfo@gmail.com>
2023-04-02 21:52:21 -0700

    personnel updatedm but not tested. reimbursement missing update and delete. nalso not tested

--- Project1/app/controllers/personnel_controller.rb
+++ Project1/app/controllers/personnel_controller.rb
@@ -1,18 +1,61 @@
 class PersonnelController < ApplicationController
   include Authenticate
   def index
+    if current_user.title == 'Manager'
+      json status: [200, 'OK'], body: {personnel: Personnel.all}, headers: cors
+    else
+      json status: [401, 'Unauthorized'], body: {message: 'Invalid token'}, headers: cors
+    end
   end
 
   def create
+    if current_user.title == 'Manager'
+      new_personnel = Personnel.new(@request[:body])
+      begin
+        if new_personnel.save
+          {status: [204, 'No Content'], headers: cors({'Location' => "/personnel/#{new_personnel.id}"})}
+        else
+          json status: [400, 'Bad Request'], body: {message: 'Invalid personnel creation'}, headers: cors
+        end
+        rescue StandardError => e
+      end
+    else
+      json status: [401, 'Unauthorized'], body: {message: 'Invalid token'}, headers: cors
+    end
   end
 
   def show
+    personnel = Personnel.find(params[:id])
+    json status: [200, 'OK'], body: {personnel: personnel}, headers: cors
   end
 
+  #Should managers be able to update personnel information?
+  # In this code the manager can only update their own information
   def update
+    personnel = Personnel.find(params[:id])
+    begin
+      if personnel.id == current_user.id
+        personnel.update(@request[:body])
+        {status: [204, 'No Content'], headers: cors({'Location' => "/personnel/#{personnel.id}"})}
+      else
+        json status: [401, 'Unauthorized'], body: {message: 'Invalid token'}, headers: cors
+      end
+      rescue StandardError => e
+    end
   end
 
   def destroy
+    personnel = Personnel.find(params[:id])
+    @id = personnel.id
+    begin
+      if personnel.title == 'Manager'
+        personnel.destroy
+        {status: [204, 'No Content'], headers: cors({'Location' => "/personnel/#{@id}"})}
+      else
+        json status: [401, 'Unauthorized'], body: {message: 'Invalid token'}, headers: cors
+      end
+      rescue StandardError => e
+    end
   end
 
   def authenticate(password)
--- Project1/app/controllers/reimbursement_controller.rb
+++ Project1/app/controllers/reimbursement_controller.rb
@@ -1,12 +1,29 @@
 class ReimbursementController < ApplicationController
   include Authenticate
   def index
+    if current_user.title == 'Manager'
+      json status: [200, 'OK'], body: {reimbursement_requests: ReimbursementRequest.all}, headers: cors
+    else
+      json status: [200, 'OK'], body: {reimbursement_requests: ReimbursementRequest.where(personnel_id: current_user.id)}, headers: cors
+    end
   end
 
   def create
+    new_reimbursement = ReimbursementRequest.new(@request[:body])
+    begin
+      if new_reimbursement.save
+        {status: [204, 'No Content'], headers: cors({'Location' => "/reimbursement/#{new_reimbursement.id}"})}
+      else
+        json status: [400, 'Bad Request'], body: {message: 'Invalid reimbursement request'}, headers: cors
+      end
+      rescue StandardError => e
+    end
   end
 
+  #Authentication should handle this so that only the user who created the reimbursement request can see it
   def show
+    reimbursement = ReimbursementRequest.find(params[:id])
+    json status: [200, 'OK'], body: {reimbursement_request: reimbursement}, headers: cors
   end
 
   def update
