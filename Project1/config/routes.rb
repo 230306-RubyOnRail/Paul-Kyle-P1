a3a25240facdda2e6cf267b2a6b22067b95c37ad
Paul Smith <paulsmith.contactinfo@gmail.com>
2023-04-07 08:29:32 -0700

    changed login routing

--- Project1/config/routes.rb
+++ Project1/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  post 'auth/login', to: 'session#create'
+  post 'login', to: 'session#create'
   resources :personnel, only: [:index, :create, :show, :update, :destroy]
   resources :reimbursement, only: [:index, :create, :show, :update, :destroy]
 
